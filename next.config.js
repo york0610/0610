6d0cb83974f789d538b274a129742146200d99b5
Your Name <your-email@example.com>
2025-10-30 16:04:27 +0800

    fix: 更新配置解決 Netlify 部署問題

---
+++ next.config.js
@@ -0,0 +1,23 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: { 
+    unoptimized: true 
+  },
+  compiler: { 
+    styledComponents: true 
+  },
+  output: 'standalone',
+  experimental: {
+    swcMinify: true,
+    optimizePackageImports: [
+      'react-icons/fa',
+      'framer-motion',
+      '@fortawesome/fontawesome-svg-core',
+      '@fortawesome/free-solid-svg-icons',
+      '@fortawesome/react-fontawesome',
+    ]
+  },
+}
+
+module.exports = nextConfig
